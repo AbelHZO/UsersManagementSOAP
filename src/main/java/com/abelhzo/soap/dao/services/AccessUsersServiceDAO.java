90a60ff14a8f49f7de7e390d042abdaa68149a9b
AbelHZO <abel26.hernandez@gmail.com>
2017-05-11 01:07:36 -0500

    Se le dio formato al codigo.

--- src/main/java/com/abelhzo/soap/dao/services/AccessUsersServiceDAO.java
+++ src/main/java/com/abelhzo/soap/dao/services/AccessUsersServiceDAO.java
@@ -3,10 +3,13 @@ package com.abelhzo.soap.dao.services;
 import com.abelhzo.jpa.Accessuser;
 
 public interface AccessUsersServiceDAO {
-	
+
 	public Accessuser saveAccessUsers(Accessuser accessuser);
+
 	public Integer logoutAccessUsers(Accessuser accessuser);
+
 	public Accessuser isActiveSessionUser(Accessuser accessuser);
+
 	public void deleteLastAccess(Accessuser accessuser);
 
 }
--- src/main/java/com/abelhzo/soap/dao/services/RolesServiceDAO.java
+++ src/main/java/com/abelhzo/soap/dao/services/RolesServiceDAO.java
@@ -7,8 +7,11 @@ import com.abelhzo.jpa.Role;
 public interface RolesServiceDAO {
 
 	public Role saveRol(Role role);
+
 	public Role updateRol(Role role);
+
 	public Role getRol(Long idrole);
+
 	public List<Role> listRol();
 
 }
